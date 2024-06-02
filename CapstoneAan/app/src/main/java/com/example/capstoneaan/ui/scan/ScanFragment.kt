547d22b32398eb81a8fe940a2466c04c1a1aa300
Andrian Tambunan <andriantambunan2752@gmail.com>
2024-06-03 04:31:55 +0700

    Add Navigation Bar

---
+++ CapstoneAan/app/src/main/java/com/example/capstoneaan/ui/scan/ScanFragment.kt
@@ -0,0 +1,37 @@
+package com.example.capstoneaan.ui.scan
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.capstoneaan.databinding.FragmentScanBinding
+
+class ScanFragment : Fragment() {
+
+    private var _binding: FragmentScanBinding? = null
+    private val binding get() = _binding!!
+    private val scanViewModel: ScanViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentScanBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textScan
+        scanViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
